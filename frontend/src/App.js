529cfb0821d92ea359195deccd19b84f87759053
Praba <prama1303@gmail.com>
2022-08-08 21:40:06 +0200

    react-router settings home and Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className='App'>
-      <h1>Lets Start with frontend</h1>
+      <BrowserRouter>
+        <Navbar />
+        <div className='pages'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
